c8c226b461ae006716be8c96dd7e9f4040bd7706
NoNameX <NoNameX@NoName>
2016-02-18 15:42:09 +0000

    Sub Done

--- src/calculator/Demo.java
+++ src/calculator/Demo.java
@@ -15,7 +15,7 @@ public class Demo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Sum: "+4+1);
+        System.out.println("Sub: "+(4-1));
     }
     
 }
